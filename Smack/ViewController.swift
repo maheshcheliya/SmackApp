583c18814482f63fe3b08f196c5ff57901f8cace
Mahesh <maheshsheliya@gmail.com>
2020-10-14 23:06:41 -0400

    initial commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Mahesh on 14/10/20.
+//  Copyright © 2020 Sheliya Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
