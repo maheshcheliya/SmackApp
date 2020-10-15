05073471ddbbdb1670458847f4f930756298b00b
Mahesh <maheshsheliya@gmail.com>
2020-10-15 01:54:35 -0400

    lesson-05

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,21 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Mahesh on 15/10/20.
+//  Copyright © 2020 Sheliya Infotech. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+}
--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -19,16 +19,7 @@ class LoginVC: UIViewController {
     @IBAction func closePressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func createAccountBtnPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
-    */
-
 }
